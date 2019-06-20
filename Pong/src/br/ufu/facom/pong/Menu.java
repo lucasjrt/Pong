7e1f43817f358bab5ea42d5b952e16a909a48892
Lucasjrt <lucasjrt@outlook.com>
2019-06-20 15:09:18 -0300

    Menu funcionando

--- Pong/src/br/ufu/facom/pong/Menu.java
+++ Pong/src/br/ufu/facom/pong/Menu.java
@@ -24,6 +24,16 @@ public class Menu extends JPanel{
 	private Image img;
 	private Graphics g;
 	
+	private String[] titulos = {"Estilo de jogo", "Modo de jogo", "Velocidade", "Tamanho do bloco"}; 
+	public String[][] conteudos = 
+			{{"Tênis", "Futebol", "Paredão"},
+			{"Jogo", "Treino"},
+			{"Lento", "Médio", "Rápido", "Crescente"},
+			{"Pequeno", "Médio", "Grande"}};
+	
+	public int[] selecionado = {0, 0, 1, 1}; // Posição do vetor de cada opção que está selecionada
+	public int atual = 0; // Opção do menu selecionada para ser modificada
+	
 	public Menu() {
 		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
 		inicializarFrame();
@@ -40,12 +50,12 @@ public class Menu extends JPanel{
 			}
 			public void keyPressed(KeyEvent e) {}
 		});
-		desenhaMenu(g);
+		addKeyListener(new TecladoMenu(this));
+		atualizaMenu();
 	}
 	
 	private void inicializar() {
 		frame = new JFrame("Pong");
-		System.out.println(LARGURA_TELA + ", " + ALTURA_TELA);
 		img = createImage(LARGURA_TELA, ALTURA_TELA);
 		g = img.getGraphics();
 	}
@@ -63,19 +73,61 @@ public class Menu extends JPanel{
 	}
 	
 	private void desenhaMenu(Graphics g) {
-		Font f = new Font("monospace", Font.PLAIN, 128);
+		Font f = new Font("monospace", Font.PLAIN, 256);
 		FontRenderContext frc = new FontRenderContext(null,true, true);
+		g.setColor(Color.black);
 		g.fillRect(0, 0, LARGURA_TELA, ALTURA_TELA);
 		g.setColor(Color.white);
 		desenhaTitulo(g, f, frc);
+		f = new Font("monospace", Font.PLAIN, 64);
+		g.setFont(f);
+		desenhaCelulas(g, f, frc);
 	}
 	
 	private void desenhaTitulo(Graphics g,Font f, FontRenderContext frc) {
 		Rectangle2D r = f.getStringBounds("Pong", frc);
-		int xString = (int) ((getWidth() - r.getWidth()) / 2.0 );
-		int yString = (int) ((getHeight() + r.getHeight()) / 2.0);
+		int x = (int) (getWidth() - r.getWidth()) >> 1;
+		int y = 3 * (((int) (getHeight() + r.getHeight())) >> 4);
 		g.setFont(f);
-		g.drawString("Pong", xString, yString);
+		g.drawString("Pong", x, y);
+	}
+	
+	private void desenhaCelulas(Graphics g, Font f, FontRenderContext frc) {
+		int espacamento = calculaEspacamento(titulos, f, frc);
+		Rectangle2D r;
+		int y = 3 * ((int) (getHeight()) >> 2);
+		int x, temp, acumulado = espacamento;
+		for(int i = 0; i < titulos.length; i++) {
+			r = f.getStringBounds(titulos[i], frc);
+			x = acumulado;
+			Color c = g.getColor();
+			if(i == atual)
+				g.setColor(Color.blue);
+			else
+				g.setColor(Color.black);
+			g.fillRect(x, y + (int)r.getY(), (int)r.getWidth() + 5, 3 * (int)r.getHeight());
+			g.setColor(c);
+			g.drawString(titulos[i], x, y);
+			g.drawRect(x, y + (int) r.getY(), (int)r.getWidth() + 5, 3 * (int)r.getHeight());
+			temp = (int) f.getStringBounds(conteudos[i][selecionado[i]], frc).getWidth();
+			x = ((int) r.getWidth() >> 1) - (temp >> 1) + acumulado;
+			g.drawString(conteudos[i][selecionado[i]], x, y + 100);
+			acumulado += r.getWidth() + espacamento;
+		}
+	}
+	
+	public void atualizaMenu() {
+		desenhaMenu(g);
+		repaint();
+	}
+	
+	private int calculaEspacamento(String[] vetor, Font f, FontRenderContext frc) {
+		int espacamento = 0, i;
+		for(i = 0; i < vetor.length; i++)
+			espacamento += f.getStringBounds(vetor[i], frc).getWidth();
+		espacamento = getWidth() - espacamento;
+		espacamento = espacamento / (i + 1);
+		return espacamento;
 	}
 	
 	public void paint(Graphics g) {
